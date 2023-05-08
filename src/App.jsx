9d729230a634ebd829041496d9c2e6152cc0e293
rosemutai <chepngetichrose2030@gmail.com>
2023-05-08 21:49:10 +0300

    Display cart context

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import CartContainer from './components/CartContainer';
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CartContainer from './components/CartContainer'
 import Navbar from './components/Navbar'
+import { calculateTotals } from './features/cart/cartSlice'
+import Modal from './components/Modal'
 
 function App() {
+
+  const dispatch = useDispatch()
+  const { cartItems } = useSelector((state) => state.cart)
+  const { isOpen } = useSelector((state) => state.modal)
+
+  useEffect(() => {
+    dispatch(calculateTotals())
+  }, [cartItems, dispatch])
   return (
     <main>
+      { isOpen && <Modal /> }
       <Navbar />
       <CartContainer />
     </main>
