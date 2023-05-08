f02de589246a87ff18cdb0c0cf439d966060a9b4
rosemutai <chepngetichrose2030@gmail.com>
2023-05-08 16:20:44 +0300

    Add cart container

---
+++ src/components/CartContainer.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux'
+import CartItem from './CartItem';
+
+const CartContainer = () => {
+  const { cartItems, amount, total} = useSelector((state) => state.cart)
+
+  if(amount < 1 ) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    )
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CartItem key={item.id} {...item} />
+        })}
+      </div>
+    </section>
+  );
+}
+
+export default CartContainer
